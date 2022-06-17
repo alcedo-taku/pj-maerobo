6c8b6c7fbc99c87a0789e9d21b658709bcac552e
立脇拓 <takuji.coroton.thanks@gmail.com>
2022-06-18 07:55:45 +0900

    feat: wrapper を追加

---
+++ Core/Src/wrapper.cpp
@@ -0,0 +1,30 @@
+#include "wrapper.hpp"
+
+/* Include Begin */
+/* Include End */
+
+/* Enum Begin */
+/* Enum End */
+
+/* Struct Begin */
+/* Struct End */
+
+/* Variable Begin */
+/* Variable End */
+
+/* Class Constructor Begin */
+/* Class Constructor End */
+
+/* Function Prototype Begin */
+/* Function Prototype End */
+
+void init(void){
+
+}
+
+void loop(void){
+
+}
+
+/* Function Body Begin */
+/* Function Body End */
