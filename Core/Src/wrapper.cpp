3d48259e3708c7c284cbfb6832fa5d9e49d16ff7
立脇拓 <takuji.coroton.thanks@gmail.com>
2022-06-19 11:10:13 +0900

    feat: すべてのシフトレジスタの動作確認完了

--- Core/Src/wrapper.cpp
+++ Core/Src/wrapper.cpp
@@ -1,8 +1,12 @@
 #include "wrapper.hpp"
 
 /* Include Begin */
+#include <iostream>
+#include <string>
+#include <vector>
 #include "gpio.h"
 #include "spi.h"
+#include "tim.h"
 #include "DUALSHOCK2.hpp"
 #include "TC74HC595.hpp"
 /* Include End */
@@ -11,6 +15,10 @@
 /* Enum End */
 
 /* Struct Begin */
+struct Timer{
+    TIM_HandleTypeDef *htim;
+    uint32_t channel;
+};
 /* Struct End */
 
 /* Variable Begin */
@@ -18,7 +26,18 @@ std::array<uint8_t, 21> receive_data;
 std::array<int16_t, 4> stick_data;
 uint8_t debug_receive_data[21];
 int16_t debug_stick_data[4];
-uint8_t sr_data;
+uint16_t sr_data;
+const std::vector<Timer> md_pwm = {	{&htim4,  TIM_CHANNEL_1},
+									{&htim4,  TIM_CHANNEL_2},
+									{&htim15, TIM_CHANNEL_1},
+									{&htim15, TIM_CHANNEL_2},
+									{&htim12, TIM_CHANNEL_1},
+									{&htim12, TIM_CHANNEL_2},
+									{&htim13, TIM_CHANNEL_1},
+									{&htim14, TIM_CHANNEL_1} };
+std::array<int16_t, 8> md_compare;
+
+//Timer md_pwm[2] = {{&htim4, TIM_CHANNEL_1}, {&htim4,  TIM_CHANNEL_2}};
 /* Variable End */
 
 /* Class Constructor Begin */
@@ -32,6 +51,10 @@ tc74hc595::TC74HC595 tc74hc595_writer({SR_SI_GPIO_Port, SR_SI_Pin}, {SR_SCK_GPIO
 void init(void){
 	dualshock2.init();
 	dualshock2.reset_stick();
+	tc74hc595_writer.init();
+	for (uint8_t i=0; i < 8; i++) {
+		HAL_TIM_PWM_Start(md_pwm[i].htim, md_pwm[i].channel);
+	}
 }
 
 void loop(void){
@@ -41,11 +64,31 @@ void loop(void){
 	stick_data = dualshock2.get_stick_data();
 	std::copy(stick_data.begin(), stick_data.end(), debug_stick_data);
 
-	for ( int i=0; i<8; i++ ) {
+	md_compare[2] = stick_data[0]*8;
+
+//	sr_data = 0;
+//	for (uint8_t i=0; i < 8; i++) {
+//		if(md_compare[i] >= 0){
+//			__HAL_TIM_SET_COMPARE(md_pwm[i].htim, md_pwm[i].channel, md_compare[i]);
+//			sr_data += 1<<(i*2);
+//		}else{
+//			__HAL_TIM_SET_COMPARE(md_pwm[i].htim, md_pwm[i].channel, -md_compare[i]);
+//			sr_data += 1<<(i*2+1);
+//		}
+//	}
+//	tc74hc595_writer.update((uint8_t*)&sr_data, 2);
+
+	for ( uint8_t i=0; i<16; i++ ) {
 		sr_data = (1<<i);
-		tc74hc595_writer.update(&sr_data, 1);
+		tc74hc595_writer.update((uint8_t*)&sr_data, 2);
+//		HAL_GPIO_WritePin( SR_RCK_GPIO_Port, SR_RCK_Pin, GPIO_PIN_RESET );
+//		tc74hc595_writer.shift_out(tc74hc595::LSBFIRST, sr_data>>8);
+//		tc74hc595_writer.shift_out(tc74hc595::LSBFIRST, sr_data);
+//		HAL_GPIO_WritePin( SR_RCK_GPIO_Port, SR_RCK_Pin, GPIO_PIN_SET );
 		HAL_Delay(200);
 	}
+
+	HAL_Delay(100);
 
 	static uint8_t led_count = 0;
 	if(led_count > 2){
