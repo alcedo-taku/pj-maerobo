26dcab39168929924acb1083b5f8c6b9e79ecdff
立脇拓 <takuji.coroton.thanks@gmail.com>
2022-06-19 19:10:25 +0900

    feat: 全モータ動作確認完了

--- Core/Src/wrapper.cpp
+++ Core/Src/wrapper.cpp
@@ -27,8 +27,8 @@ std::array<int16_t, 4> stick_data;
 uint8_t debug_receive_data[21];
 int16_t debug_stick_data[4];
 uint16_t sr_data;
-const std::vector<Timer> md_pwm = {	{&htim4,  TIM_CHANNEL_1},
-									{&htim4,  TIM_CHANNEL_2},
+const std::vector<Timer> md_pwm = {	{&htim4,  TIM_CHANNEL_4},
+									{&htim4,  TIM_CHANNEL_3},
 									{&htim15, TIM_CHANNEL_1},
 									{&htim15, TIM_CHANNEL_2},
 									{&htim12, TIM_CHANNEL_1},
@@ -36,6 +36,7 @@ const std::vector<Timer> md_pwm = {	{&htim4,  TIM_CHANNEL_1},
 									{&htim13, TIM_CHANNEL_1},
 									{&htim14, TIM_CHANNEL_1} };
 std::array<int16_t, 8> md_compare;
+int16_t debug_md_compare[8];
 
 //Timer md_pwm[2] = {{&htim4, TIM_CHANNEL_1}, {&htim4,  TIM_CHANNEL_2}};
 /* Variable End */
@@ -58,37 +59,41 @@ void init(void){
 }
 
 void loop(void){
-	dualshock2.update(0x20);
+	dualshock2.update(0x30);
 	receive_data = dualshock2.get_data_exex();
 	std::copy(receive_data.begin(), receive_data.end(), debug_receive_data);
 	stick_data = dualshock2.get_stick_data();
 	std::copy(stick_data.begin(), stick_data.end(), debug_stick_data);
 
-	md_compare[2] = stick_data[0]*8;
-
-//	sr_data = 0;
-//	for (uint8_t i=0; i < 8; i++) {
-//		if(md_compare[i] >= 0){
-//			__HAL_TIM_SET_COMPARE(md_pwm[i].htim, md_pwm[i].channel, md_compare[i]);
-//			sr_data += 1<<(i*2);
-//		}else{
-//			__HAL_TIM_SET_COMPARE(md_pwm[i].htim, md_pwm[i].channel, -md_compare[i]);
-//			sr_data += 1<<(i*2+1);
-//		}
-//	}
-//	tc74hc595_writer.update((uint8_t*)&sr_data, 2);
-
-	for ( uint8_t i=0; i<16; i++ ) {
-		sr_data = (1<<i);
-		tc74hc595_writer.update((uint8_t*)&sr_data, 2);
-//		HAL_GPIO_WritePin( SR_RCK_GPIO_Port, SR_RCK_Pin, GPIO_PIN_RESET );
-//		tc74hc595_writer.shift_out(tc74hc595::LSBFIRST, sr_data>>8);
-//		tc74hc595_writer.shift_out(tc74hc595::LSBFIRST, sr_data);
-//		HAL_GPIO_WritePin( SR_RCK_GPIO_Port, SR_RCK_Pin, GPIO_PIN_SET );
-		HAL_Delay(200);
+	for (uint8_t i = 0; i < 4; i++) {
+		md_compare[i] = stick_data[i]*8;
 	}
+	md_compare[4] = (receive_data[9] -receive_data[10])*3;
+	md_compare[5] = (receive_data[11]-receive_data[12])*3;
+	md_compare[6] = (receive_data[17]-receive_data[19])*3;
+	md_compare[7] = (receive_data[18]-receive_data[20])*3;
 
-	HAL_Delay(100);
+	std::copy(md_compare.begin(), md_compare.end(), debug_md_compare);
+
+	sr_data = 0;
+	for (uint8_t i=0; i < 8; i++) {
+		if(md_compare[i] >= 0){
+			__HAL_TIM_SET_COMPARE(md_pwm[i].htim, md_pwm[i].channel, md_compare[i]);
+			sr_data += 1<<(i*2);
+		}else{
+			__HAL_TIM_SET_COMPARE(md_pwm[i].htim, md_pwm[i].channel, -md_compare[i]);
+			sr_data += 1<<(i*2+1);
+		}
+	}
+	tc74hc595_writer.update((uint8_t*)&sr_data, 2);
+
+//	for ( uint8_t i=0; i<16; i++ ) {
+//		sr_data = (1<<i);
+//		tc74hc595_writer.update((uint8_t*)&sr_data, 2);
+//		HAL_Delay(200);
+//	}
+
+	HAL_Delay(200);
 
 	static uint8_t led_count = 0;
 	if(led_count > 2){
